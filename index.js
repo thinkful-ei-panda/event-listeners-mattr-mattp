333843e7d1716178a9845bf15fcbe145e3d508f6
Matt <63121708+themlp101@users.noreply.github.com>
2020-06-02 12:44:20 -0400

    finisehd js file with jQuery

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-$('.thumbnail').on('click', e => {
-  console.log(e.target);
-});
+/* eslint-disable no-undef */
+/* eslint-disable strict */
+
+function handleThumbnailClicks() {
+  $('.thumbnail').click(function (event) {
+    const imgSrc = $(event.currentTarget).find('img').attr('src');
+    const imgAlt = $(event.currentTarget).find('img').attr('alt');
+
+    // $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+
+    $('.hero img').attr({'src': imgSrc, 'alt': imgAlt});
+
+  });
+
+
+}
+
+$(handleThumbnailClicks);
