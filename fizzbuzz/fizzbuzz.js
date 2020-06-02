8136b5401d64d4a10ffdca29f23ed4a2b1e393ca
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-02 14:35:34 -0700

    Update fizzbuzz.js

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,29 +1,39 @@
-/* eslint-disable no-undef */
 /* eslint-disable strict */
 
-function playGame() {
-  //On submit get "number-choice"
-  $('#number-chooser').submit(function(event){
+$(function() {
+  $('#number-chooser').submit( event => {
     event.preventDefault();
-    // prevents default action from happening, ie load to a new page
-    const choosenNumber = $(this).find('#number-choice').val();
-    // store the number the user inputted into choosenNumber variable
-    // for each number
-    // 1. check whether it is divisible by 3, 5 or both
-    // 2. append correct format to js-results div
-    for (let counter = 0; counter <= choosenNumber; counter++){
-      if (counter % 3 === 0 && counter % 5 === 0) {
-        $('.js-results').append('<div class="fizz-buzz-item fizzbuzz"><span>Fizzbuzz</span></div>');
-      } else if (counter % 5 === 0) {
-        $('.js-results').append('<div class="fizz-buzz-item fizz"><span>Fizz</span></div>');
-      } else if(counter % 3 === 0) {
-        $('.js-results').append('<div class="fizz-buzz-item buzz"><span>Buzz</span></div>');
-      } else {
-        $('.js-results').append(`<div class="fizz-buzz-item"><span>${counter}</span></div>`).css('fizz-buzz-item');
+    $(".js-results").empty();
+    const num = parseInt( $(event.currentTarget).find(
+      'input[name="number-choice"]').val());
+   
+    const results = [];
+    
+    for (let i = 1; i <= num; i ++) {
+      
+      
+      if (i % 15 === 0) {
+        results.push($(
+          '<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>'));  
+      }
+      
+      else if (i % 5 === 0) {
+        results.push($(
+          '<div class="fizz-buzz-item buzz"><span>buzz</span></div>')); 
+      }
+      
+      else if (i % 3 === 0) {
+        results.push($(
+          '<div class="fizz-buzz-item fizz"><span>fizz</span></div>')); 
+      }
+    
+      else {
+        results.push($(
+          `<div class="fizz-buzz-item"><span>${i}</span></div>`)); 
       }
     }
+  
+    $(".js-results").append(results);
   });
-}
-
-// make ready
-$(playGame);
+  
+});
